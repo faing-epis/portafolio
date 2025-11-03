35329f2b8f7111b8e171e5b1b0b535de6c7e60b0
faing-epis <portafolioepis@virtual.upt.pe>
2025-11-03 16:34:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw3SV6EvFXemwPzEaKlf8rPlc9hcMBqTqeTwMWoCJDPI2eU_kichHzNxmDiuDIudJZ0/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbznx9vMo36374ZLPm_bC0p-mUCTYFyCW6-VAFJC8j9gp5EkdBkU_FGmGq3QMjDPNwmh/exec";
 
 // --- Selectores de Elementos DOM ---
 const semesterModal = document.getElementById('semesterModal');
@@ -1228,4 +1228,5 @@ saveFinalReportBtn.addEventListener('click', async () => {
         saveFinalReportBtn.innerHTML = originalButtonText;
         saveFinalReportBtn.disabled = false;
     }
+
 });
